d2463fca7470183edf9d8da76626c70104ca6cb2
TimothyMark17 <tim_mark2000@hotmail.com>
2020-08-03 10:52:39 +0800

    Editing the assignment R script for Peer graded assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y)  {
+          x <<- y
+          inv <<- NULL
+    }
+    get <- function() {x}                               ## Getting the value of the matrix
+    setInverse <- function(inverse) {inv <<- inverse}   ##Setting the value of the inverse
+    getInverse <<- function() {inv}                     ##Getting the value of the inverse
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
+
+
+
 
 
 ## Write a short comment describing this function
 
+## Computes the inverse of a matrix; matrix gotten from the functions above. 
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inv <- x$getInverse()                         
+      if(!is.null(inv)){                  ##To determine if the inverse has been calculated and cached
+          message("Getting cached data")
+          return(inv)                     ##Returns the inverse
+      }
+      mat <- x$get()          
+      inv <- solve(mat, ...)  ## Gives inverse of mat    
+      x$setInverse(inv)        ## Setting the value of the result in the cache
+      return(inv)             ## Finally returns inv
 }
